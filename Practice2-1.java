48ae675442e28ee36fa3511aa94966c356f41242
yueyy <31062615+yueyy@users.noreply.github.com>
2017-10-22 19:36:04 +0800

    Create Practice2-1.java

---
+++ Practice2-1.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+
+    char [] num = {'1','2','4'};
+
+    public static void main(String[] args) {
+
+        Main print = new Main();
+        System.out.print(print.num);
+    }
+}
+
+/**
+* 很迷
+
+* 我只会这种
+
+* 毫无技术含量的233333
+
+*/
+
+import java.util.*;
+
+public class Main {
+
+    int [] num = {1, 3,4,56,78};
+
+    public void print()
+    {
+        for (int i=0;i<num.length;i++)
+            System.out.println(num[i]);
+    }
+
+
+    public static void main(String[] args) {
+        
+        Main a = new Main();
+        a.print();
+
+    }
+}
