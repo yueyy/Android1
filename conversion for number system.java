86ec2a8278f064dbc66dad6305d5cee83c6b02dc
yueyy <31062615+yueyy@users.noreply.github.com>
2017-10-27 07:52:42 +0800

    Update conversion for number system.java

--- conversion for number system.java
+++ conversion for number system.java
@@ -1,49 +1,27 @@
 import java.util.*;
+import static java.lang.Integer.parseInt;
 
 public class Main {
 
-    public static int abs(int n)
+    public static int parselnt(String s,int radix)
     {
-        if (n<0){
-            return -n;
-        }else {
-            return n;
-        }
+        return parseInt(s,10);
     }
 
-    public static double change(int in,int out,double num)
-    {
-        int integer = (int)num;
-        double doubles = num - integer;
-        if(in==out)
-        {
-            return num;
-        }else
-        {
-            String strnum = String.valueOf(integer);
-            int l = strnum.length();
-            num = num - num;
-            for(int i=0; i<l ; i++)
-            {
-                int a = integer%10;
-                num = (int)Math.pow(in,i) + num;
-                a = integer/10;
-            }
-            return num;
-        }
-    }
 
     public static void main(String[] args) {
-        Scanner in =new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
         int number = in.nextInt();
 
-        number = abs(number);
-        System.out.println(change(a,b,number));
+        number = Math.abs(number);
+        String num = String.valueOf(number);
+        number = parselnt(num,10);
+
+        String a = Integer.toString(Integer.valueOf(num),n);
+        System.out.println(a);
+
     }
 }
 
-
-// 暂时只能计算整数 晚上再改进！！！
-// emmmmm有bug……零的问题 电脑没电了……晚上再改正！！！
+// 暂时只能计算整数 再继续改进！！！
